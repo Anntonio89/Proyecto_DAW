76cf6c7fe1d6c57d496bf439c27948055e6b395a
Anntonio89 <ant_ma_ol@outlook.es>
2025-08-08 12:20:48 +0200

    Responsive

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import {useEffect, useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import storage from '../utils/storage'
-import {FaUserCircle} from 'react-icons/fa'
+import {FaUserCircle, FaBars, FaTimes} from 'react-icons/fa'
 
 function Header({filtro, setFiltro}) {
   const [userLogued, setUserLogued] = useState(null)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   useEffect(()=>{
     const user=storage.get('authUser')
@@ -17,19 +18,26 @@ function Header({filtro, setFiltro}) {
   return (
     <>
     <header className='header'>
-        <nav className='nav'>
-            <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}>Inicio</NavLink>
-            <NavLink to='/services' className={({ isActive }) => (isActive ? 'active' : '')}>Qué ofrecemos</NavLink>
-            <NavLink to='/contact'className={({ isActive }) => (isActive ? 'active' : '')}>Contacto</NavLink>
+      <button className='desplegar' onClick={()=> setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+        <nav className={`nav ${menuOpen ? 'nav-open' : ''}`}>
+            <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')} onClick={() => setMenuOpen(false)}>Inicio</NavLink>
+            <NavLink to='/services' className={({ isActive }) => (isActive ? 'active' : '')} onClick={() => setMenuOpen(false)}>Qué ofrecemos</NavLink>
+            <NavLink to='/contact'className={({ isActive }) => (isActive ? 'active' : '')} onClick={() => setMenuOpen(false)}>Contacto</NavLink>
             {/*Acceder (Login) / Ver perfil según logueado*/}
-            {!userLogued ? <NavLink to='/login'className={({ isActive }) => (isActive ? 'active' : '')}>Acceder</NavLink> 
-                        : <NavLink to='/login'className={({ isActive }) => (isActive ? 'active' : '')}>
+            {!userLogued ? (<NavLink to='/login'className={({ isActive }) => (isActive ? 'active' : '')} onClick={() => setMenuOpen(false)}>Acceder</NavLink>)
+                        : (<NavLink to='/login'className={({ isActive }) => (isActive ? 'active' : '')}>
                             <FaUserCircle style={{marginRight:'5px'}}/>
                             {userLogued.nombre}<p style={{marginLeft: '25px',fontSize:'12px'}}>{userLogued.rol}</p>
                           </NavLink>
-            }
+            )}
+            <div className='busqueda-mobile'>
+              <input type='text' placeholder='Buscar...' value={filtro} onChange={(e)=>setFiltro(e.target.value)}/>
+            </div>
         </nav>
-        <div className='busqueda'>
+        {/* Buscador para desktop */}
+        <div className='busqueda-desktop'>
             <input type='text' placeholder='Buscar...' value={filtro} onChange={(e)=>setFiltro(e.target.value)}/>
         </div>
     </header>   
